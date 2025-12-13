bd6e8fa9b111224ce41549b34a353f1efea5e4a3
Raunak <raunakswain.19402@gmail.com>
2025-12-13 09:21:56 +0530

    Created Landing Page

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,19 +1,61 @@
 import React from 'react'
-import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from '@clerk/nextjs'
+import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/nextjs'
+import Link from 'next/link'
+import Image from 'next/image'
+import { Button } from './ui/button'
+import { LayoutDashboard, PenBox } from 'lucide-react'
 
 const Header = () => {
   return (
-    <div className='flex justify-between'>
+    <div className='fixed top-0 w-full bg-white/80 backdrop-blur-md z-50 border-b'>
+
+      <nav className='container mx-auto px-4 py-4 flex items-center justify-between'>
+        <Link href='/'>
+          <Image 
+            src = {'/cover.png'}
+            height={100}
+            width={100}
+            alt='Rupify Logo'
+            className='w-32 h-12 object-contain'
+          />
+         </Link>
+
+       <div className='flex items-center space-x-4'>
+        <SignedIn>
+          <Link href='/dashboard' className='text-gray-600 hover:text-blue-600 flex items-center gap-2'>
+             <Button variant={'outline'}>
+                     <LayoutDashboard size={18} />
+                      <span className='hidden md:inline'>Dashboard</span>   
+             </Button>
+          </Link>
+
+          <Link href='/transaction/create' className='text-gray-600 hover:text-blue-600 flex items-center gap-2'>
+             <Button>
+                     <PenBox size={18} />
+                      <span className='hidden md:inline'>Add Transaction</span>   
+             </Button>
+          </Link>
+
+        </SignedIn>
         {/* agar user signed out hai  */}
         <SignedOut>
-              <SignInButton />
-              <SignUpButton />
+              <SignInButton forceRedirectUrl='/dashboard'>
+                 <Button variant={'outline'}>
+                  Login
+                 </Button>
+              </SignInButton>
         </SignedOut>
         
         {/* agar user signed in hai */}
         <SignedIn>
-              <UserButton />
+              <UserButton appearance={{
+                elements:{
+                  avatarBox:"w-10 h-10"
+                }
+              }}/>
         </SignedIn>
+       </div>
+      </nav>
     </div>
   )
 }
